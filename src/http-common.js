99c9182dd1492406949bdb2af68fd52aea9842e8
Dhairya19 <47753091+Dhairya19@users.noreply.github.com>
2021-06-24 01:12:37 +0530

    Add files via upload

---
+++ src/http-common.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default axios.create({
+  method:'OPTIONS, PUT, POST, GET, DELETE',
+  baseURL: "http://localHost:8080",
+  headers: { 'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin' : '*',
+  'Accept': 'application/json',
+  'Vary':'Origin',
+
+ }
+});
